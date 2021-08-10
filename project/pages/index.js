1acc8ad8da09ff346cf93ff841b285cc68fc8b05
BossNumber1 <dimaang23@gmail.com>
2021-08-10 23:05:07 +0300

    Did hide and show the verse when you click on the button / block.

--- project/pages/index.js
+++ project/pages/index.js
@@ -20,10 +20,79 @@ import eight from "./about/8.jpg";
 import signa from "./about/signa.jpg";
 
 import firstIll from "./illustrations/1.jpg";
+import bgim from "./bgim.jpg";
 
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+    const [isShowVerse, setShowVerse] = React.useState(false);
+
+    let verseStore = {
+        content: `Ах! Сколько в этом взгляде красоты
+                                        <br />и нежности, дарованной богами!
+                                        <br />
+                                        Да, каждый раз при встрече только ты
+                                        <br />
+                                        мне улыбнёшься милыми глазами. <br />
+                                        Они уже о многом рассказали. <br />
+                                        Твой смех меня спасает в трудный миг.
+                                        <br />
+                                        Прекраснее всех трелей соловьиных,
+                                        <br />
+                                        он рай в моей груди, как храм, воздвиг -
+                                        <br />
+                                        масштабнее да ярче, чем Афины. <br />
+                                        <br />В час горя, как обычно, прибегу
+                                        <br />
+                                        в покои, оживлённые цветами - <br />в
+                                        твой дом из счастья, неги - всю пургу,
+                                        <br />
+                                        всю вьюгу из души смахнуть словами.
+                                        <br />
+                                        Там ангелы кружатся рядом с нами. <br />
+                                        <br />Я помню голос добрый - он лишь
+                                        твой,
+                                        <br />
+                                        единственный на свете, самый лучший!
+                                        <br />А если вдаль уйдёшь, то за тобой
+                                        <br />я ринусь хоть по морю, хоть по
+                                        суше. <br />
+                                        <br />
+                                        Знай, если вдруг улыбка пропадёт, <br />
+                                        набросятся все беды мирозданья; <br />
+                                        тлен, грусть, обид жестокий хоровод
+                                        <br />
+                                        тебя поволокут; все испытанья <br />
+                                        накинутся, как волки; то не дам я <br />
+                                        <br />
+                                        им зло тебе чинить, ведь навсегда <br />
+                                        в долгу перед тобой, моя родная...
+                                        <br />
+                                        <br /> И, как бы не меняли нас года,
+                                        <br />
+                                        тебя ни на кого не променяю!`,
+    };
+
+    const showPost = () => {
+        debugger;
+        // document.getElementById("krut").onclick = () => {
+        document.body.style.background = "gray";
+        // if (document.body.style.background === "white") {
+        //     debugger;
+        //     document.body.style.background = "gray";
+        // } else {
+        //     debugger;
+        //     document.body.style.background = "white";
+        // }
+        // };
+        setShowVerse(true);
+    };
+
+    const hidePost = () => {
+        document.body.style.background = "white";
+        setShowVerse(false);
+    };
+
     return (
         <div>
             <Head>
@@ -238,65 +307,40 @@ export default function Home() {
 
                                     // }}
                                     >
-                                        <b>Самый свежий стих</b>
+                                        <b>Ах!</b>
                                         <br />
                                         <br />
-                                        Ах! Сколько в этом взгляде красоты{" "}
-                                        <br />и нежности, дарованной богами!{" "}
-                                        <br />
-                                        Да, каждый раз при встрече только ты{" "}
-                                        <br />
-                                        мне улыбнёшься милыми глазами. <br />
-                                        Они уже о многом рассказали. <br />
-                                        Твой смех меня спасает в трудный миг.
-                                        <br />
-                                        Прекраснее всех трелей соловьиных,{" "}
-                                        <br />
-                                        он рай в моей груди, как храм, воздвиг -
-                                        <br />
-                                        масштабнее да ярче, чем Афины. <br />
-                                        <br />В час горя, как обычно, прибегу{" "}
-                                        <br />
-                                        в покои, оживлённые цветами - <br />в
-                                        твой дом из счастья, неги - всю пургу,{" "}
-                                        <br />
-                                        всю вьюгу из души смахнуть словами.{" "}
-                                        <br />
-                                        Там ангелы кружатся рядом с нами. <br />
-                                        <br />Я помню голос добрый - он лишь
-                                        твой,
-                                        <br />
-                                        единственный на свете, самый лучший!{" "}
-                                        <br />А если вдаль уйдёшь, то за тобой{" "}
-                                        <br />я ринусь хоть по морю, хоть по
-                                        суше. <br />
-                                        <br />
-                                        Знай, если вдруг улыбка пропадёт, <br />
-                                        набросятся все беды мирозданья; <br />
-                                        тлен, грусть, обид жестокий хоровод{" "}
-                                        <br />
-                                        тебя поволокут; все испытанья <br />
-                                        накинутся, как волки; то не дам я <br />
-                                        <br />
-                                        им зло тебе чинить, ведь навсегда <br />
-                                        в долгу перед тобой, моя родная...{" "}
-                                        <br />
-                                        <br /> И, как бы не меняли нас года,{" "}
-                                        <br />
-                                        тебя ни на кого не променяю!
+                                        {isShowVerse ? (
+                                            <>
+                                                <input
+                                                    type="submit"
+                                                    onClick={hidePost}
+                                                    value="Скрыть стих"
+                                                    className="btn btn-warning"
+                                                />
+                                                <div
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: verseStore.content,
+                                                    }}
+                                                ></div>
+                                            </>
+                                        ) : (
+                                            <div
+                                                className="verseCard"
+                                                onClick={showPost}
+                                            >
+                                                <img
+                                                    src={bgim}
+                                                    alt="album picture"
+                                                />
+                                                uraaa!!!
+                                            </div>
+                                        )}
                                     </div>
                                     <div className="firstIll">
                                         <img
                                             src={firstIll}
                                             alt="first_illustrations"
-                                            // style={{
-                                            //     // maxWidth: "100%",
-                                            //     // height: "auto",
-                                            //     // marginTop: 122,
-                                            //     zIndex: -1,
-                                            //     float: "left",
-                                            //     marginRight: 5,
-                                            // }}
                                         />
                                     </div>
                                 </div>
